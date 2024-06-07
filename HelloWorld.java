84423601e6643c38863ca50f89f87c7898107478
SupriseSelinda <selindasurprise22@gmail.com>
2024-06-07 22:45:11 +0200

    Fisrt

---
+++ HelloWorld.java
@@ -0,0 +1,16 @@
+public class HelloWorld{
+
+  public static void  main(String[] args){
+      
+      System.out.println("Hello"); 
+       int x=1; 
+        int y=9;
+
+          System.out.println(x +" "+"Plus"+" " +y + " is equals to"+" " + (x+y));
+
+    
+
+     }
+
+
+}
